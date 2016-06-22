a40c2e17ba52602a5f6a12ea16a0d78ce6691fda
谢靖伦 <jinglun.xie@corp.elong.com>
2016-06-22 21:08:29 +0800

    html modify

--- app/assets/javascripts/authentications.js
+++ app/assets/javascripts/authentications.js
@@ -13,29 +13,36 @@ function login(){
 }
 function register(){
   $(".error").remove();
+  $("#submit").html("正在发送注册邮件...");
   user_name = $("#user_name").val();
   email = $("#email").val();
   password = $("#password").val();
   if ($("#password_comfirm").val()==password){
     $.post("/register", {"user_name":user_name, "email":email, "password":password},function(data){
       if (data["code"]==1){
+        $("#submit").html("注册");
         window.location.href="/login";
       }
       else {
+        $("#submit").html("注册");
         insertError("#submit",data["msg"]);
       }
     })
   } else {
+    $("#submit").html("注册");
     insertError("#submit","两次密码不一致");
   }
 }
 function resetPassword(){
   $(".error").remove();
+  $("#submit").html("正在发送重置邮件...");
   email = $("#email").val();
   $.post("/password/reset", {"email":email},function(data){
     if (data["code"]==1){
+      $("#submit").html("重置密码");
       insertSuccess("#submit",data["msg"]);
     } else{
+      $("#submit").html("重置密码");
       insertError("#submit",data["msg"]);
     }
   })
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
 
+  # 流量单位转换,基数单位为B
   def k_m_g(total)
     if total<1024*1024
       return "#{(total/1024.0).round(2)}KB"
@@ -10,6 +11,7 @@ module UsersHelper
     end
   end
 
+  # 流量单位转换,基数单位为kB
   def m_g(total)
     if total<1024
       return "#{total.round(2)}KB"
